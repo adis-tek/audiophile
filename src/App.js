72824d4c1fcd05e8d83c9e388d0f56608ff3aaac
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-06-19 12:49:57 -0500

    router and default styles set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home/home.jsx';
 
 function App() {
   return (
     <div className="App">
-
+    <Router>
+      <Switch>
+      <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.scss';
+
+function home() {
+    return (
+    <>
+        <div className="home-container">
+        <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png" />
+        
+        <title>Designo Agency</title>
+        </head>
+            <body className="home-body">
+            <h1>Home Page</h1>
+            </body>
+            </div>
+    </>
+    )
+}
+
+export default home;
